eabe33c6b12884d878b7341e15544ca513f9dcfb
frazr <niklatz@gmail.com>
2015-10-07 22:48:27 +0200

    Update UnexpandTabsOnPreSave.py

--- UnexpandTabsOnPreSave.py
+++ UnexpandTabsOnPreSave.py
@@ -3,5 +3,4 @@ import sublime, sublime_plugin
 class OpenCommand(sublime_plugin.EventListener):
 	def on_pre_save(self, view):
 		if view.find("    ", 0):
-			print(view.settings())
 			view.run_command("unexpand_tabs")
